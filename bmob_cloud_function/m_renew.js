486eb7d0e258e5db1f81553640ce29842bc8c2e9
1223774014@qq.com <1223774014@qq.com>
2023-04-04 19:07:25 +0800

    update renew

--- bmob_cloud_function/m_renew.js
+++ bmob_cloud_function/m_renew.js
@@ -75,9 +75,9 @@ function onRequest(request, response, modules) {
         var moment = modules.oMoment;
         var _order = dataObject.order;
         var createdAt = moment(dataObject.createdAt);//.format('YYYY-MM-DD HH:mm:ss')
-        var expireDate = createdAt.add(_day, 'days').add(_month, 'months');
-        //response.end("createdAt = " + createdAt.format('YYYY-MM-DD HH:mm:ss') );
-        // response.end("expireDate = " + expireDate.format('YYYY-MM-DD HH:mm:ss') + ",_day = " + _day + ",_month = " + month);
+         var expireDate = createdAt.add(_day, 'days').add(_month, 'months'); //这个操作之后createAt的值会变化，坑爹
+ 
+        // response.end("expireDate = " +  moment(dataObject.createdAt).format('YYYY-MM-DD HH:mm:ss') + ",_day = " + _day + ",_month = " + month);
         if (expireDate >= moment()) { //有效期内
             //首先分析是否是会员升级
             if (dataObject.member_type == "g" && member_type == 's') {
@@ -99,11 +99,12 @@ function onRequest(request, response, modules) {
            // response.end("createdAt = " + createdAt.format('YYYY-MM-DD HH:mm:ss') + ", day = " + day + ",month = " + month);
   
         }else {
-            var diff = moment(moment()).diff(moment(expireDate), 'days'); //已经过期了，需要补全
+            //response.end();
+            var diffDays = moment(moment()).diff(moment(dataObject.createdAt), 'days'); //已经过期了，需要补齐到当前日期
             //response.end("need append diff days = " + diff);
-            _day += diff + Number(day);
-            _month += Number(month);
-           // response.end("need append diff days = " + diff + ",now month = " + _month + ",now day = " + _day);
+            _day  = diffDays + Number(day);
+            _month = Number(month);
+           // response.end("need append diff days = " + diffDays + ",now month = " + _month + ",now day = " + _day + "createdAt = " + moment(dataObject.createdAt).format('YYYY-MM-DD HH:mm:ss'));
         }
         
         if (order !== undefined) {
@@ -124,14 +125,15 @@ function onRequest(request, response, modules) {
         var uuid = data;
         var createdAt = moment(dataObject.createdAt);//.format('YYYY-MM-DD HH:mm:ss')
         var expireDate = createdAt.add(_day, 'days').add(_month, 'months').format('YYYY-MM-DD HH:mm:ss');
+       // response.end("need append diff days = " + diffDays + ",now month = " + _month + ",now day = " + _day + ",expireDate = " + expireDate);
         db.update({
         "table":"VipInfo",
         "objectId":objectId,
         "data":{"day":_day,"month":_month,'member_type':member_type,'uuid':uuid,'order':_order,'expireDate':expireDate,'version':version}
          },function(err,data){
-           //   _dataObject= JSON.parse(data);
+          //   _dataObject= JSON.parse(data);
               if (data.code === undefined) {
-                  createdAt = moment(dataObject.createdAt);//必须重新赋值一次
+                  //createdAt = moment(dataObject.createdAt);//必须重新赋值一次
                   _result.code = "A1024";
                   _result.updatedAt = data.updatedAt;
                   _result.objectId = objectId;
